b38d76bd576b00d58357a8dc017bd57de0ac2323
ShkarupaDC <dimon.shkarupa@gmail.com>
2021-04-26 15:38:01 +0300

    Move helper function to separate file

--- test.py
+++ test.py
@@ -1,61 +1,44 @@
+from selenium import webdriver
 import re
 import unittest
+from functools import partial
 from typing import NoReturn
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+from helpers import get_element_with_wait
 
 
 class HotlineTestCase(unittest.TestCase):
     def setUp(self) -> NoReturn:
         self.driver = webdriver.Firefox()
-
-    def get_with_wait(
-        self, xpath: str = None, selector: str = None, timeout: int = 10
-    ) -> WebElement:
-        if xpath is None and selector is None:
-            raise Exception("Find option is not defined")
-        try:
-            wait = WebDriverWait(self.driver, timeout)
-            locator = (
-                (By.XPATH, xpath) if xpath is not None
-                else (By.CSS_SELECTOR, selector)
-            )
-            return wait.until(EC.presence_of_element_located(locator))
-        except TimeoutException:
-            raise Exception("Could not find element")
+        self.get_element = partial(get_element_with_wait, driver=self.driver)
 
     def test_category_selection(self, category: str = "Книги") -> NoReturn:
         self.driver.get("https://hotline.ua/")
 
-        category_link = self.get_with_wait(
+        category_link = self.get_element(
             selector=f'[data-menu-main-item="{category}"] > a'
         )
         category_link.click()
 
-        header = self.get_with_wait(selector=".cell-12 > h1")
+        header = self.get_element(selector=".cell-12 > h1")
         assert header.text == category
 
     def test_filter_by_option(self, option: str = "1920-2000") -> NoReturn:
         self.driver.get("https://hotline.ua/computer/diski-ssd/")
 
-        option_link = self.get_with_wait(
+        option_link = self.get_element(
             xpath=f'//a[contains(text(), "{option}")]'
         )
         option_link.click()
 
         label = option_link.get_attribute("data-eventlabel")
-        match = re.match(r'^.*\[(\d+)\]$', label)
+        match = re.match(r"^.*\[(\d+)\]$", label)
 
         if not match:
             raise Exception(f"Invalid option {option}")
         code = match.group(1)
 
-        selected = self.get_with_wait(
+        selected = self.get_element(
             selector=f'span[data-catalog-selected-filter="{code}"]'
         )
         selected_text = selected.get_attribute("innerText")
